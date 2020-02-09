d4d052819f617bc691d064cecf95c6a057dfa851
Klara <sanchez.clara13@gmail.com>
2020-02-09 21:44:55 +0100

    corrected delete method (index not referenced)

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -2,9 +2,11 @@
   <div class="row">
     <!-- Even though the event happens on the HTML rendered in Quote.vue, we want it to be registered
     here (<app-quote>). That's why we add the "native" modifier -->
-    <app-quote v-for="quote in quotes" @click.native="deleteQuote(index)">{{
-      quote
-    }}</app-quote>
+    <app-quote
+      v-for="(quote, index) in quotes"
+      @click.native="deleteQuote(index)"
+      >{{ quote }}</app-quote
+    >
   </div>
 </template>
 
