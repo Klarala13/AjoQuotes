ac4251d703bd280a5b6d6c2ad3456a6922a69ebd
Klara <sanchez.clara13@gmail.com>
2020-02-09 20:08:01 +0100

    adds new quote

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container">
+    <new-quote @addingQuote="newQuote"></new-quote>
     <app-quote-grid v-bind:quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
+//Siempre siempre siempre siempre siempre siempre siempre siempre siempre siempre y aún así me parece poco.
+//La poesía es un arma cargada de futuro. La micropoesía es un arma cargada de pasado imperfecto. (Muy imperfecto).
+
 import Grid from "./components/Grid.vue";
+import NewQuote from "./components/NewQuote.vue";
 
 export default {
   data: function() {
@@ -16,8 +21,14 @@ export default {
       maxQuotes: 10
     };
   },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    }
+  },
   components: {
-    appQuoteGrid: Grid
+    appQuoteGrid: Grid,
+    newQuote: NewQuote
   }
 };
 </script>
