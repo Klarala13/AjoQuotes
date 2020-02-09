05822d142eb4d7344cfd554b5cb44c014da717bf
Klara <sanchez.clara13@gmail.com>
2020-02-09 19:26:22 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+import Grid from "./components/Grid.vue";
+export default {
+  data: function() {
+    return {
+      quotes: [
+        "La poesía es un arma cargada de futuro. La micropoesía es un arma cargada de pasado imperfecto. (Muy imperfecto)."
+      ],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    appQuoteGrid: Grid
+  }
+};
+</script>
+<style>
+@import "./css/style.css";
+</style>
