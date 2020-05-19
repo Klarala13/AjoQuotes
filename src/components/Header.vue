b8348c7e493d51d2d30a79d66566e1d474214ec1
unknown <clara.sanchez@wheelhub.es>
2020-05-19 16:24:44 +0200

    info for user and credit to Ajo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="row">
-    <div class="col-sm-12">
+    <div class="col-12">
       <h3>Quotes Added</h3>
+      <p class="text-center">Las citas que ves son de la maravillosa Ajo Micropoetisa. <a href="https://twitter.com/ajomicropoetisa?lang=es" target="blank">Aquí </a> puedes ver algunos de sus trabajos.</p>
       <div class="progress">
         <div class="progress">
           <div
